7f8e17ced974660b0370e6cac7a24f27e77d20a5
alex.almeida <adeno.si@gmail.com>
2017-05-21 20:20:33 -0300

    Inclusão de um template de post

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :produtos, only: [:new, :create,:destroy, :get]
   resources :leads, only: [:new, :create,:destroy, :get]
-  get "produtos/busca" => "produtos#busca", as: :busca_produto
-  
+  resources :posts, only: [:get, :new]
   root "home#index"
 end
