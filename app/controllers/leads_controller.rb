9dbec4fbf92bbef221791c71fe814a4ce7410786
alex.almeida <adeno.si@gmail.com>
2017-05-21 17:03:49 -0300

    Mais algumas mudanças, inclusão de fontes, etc

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,7 +1,7 @@
 class LeadsController < ApplicationController
     def create
+        params[:lead][:userIP] = request.remote_ip;
         newLead = params.require(:lead).permit!
         Lead.create newLead
     end
-    
 end
