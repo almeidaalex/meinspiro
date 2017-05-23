a9996318f3e3dcc169a6ed3d2a550d71c6f42dcc
alex.almeida <adeno.si@gmail.com>
2017-05-23 05:58:53 -0300

    To com sono, não aguento mais =/

--- Gemfile
+++ Gemfile
@@ -29,6 +29,12 @@ gem 'jquery-rails'
 
 gem 'mailgun-ruby', '~>1.1.6'
 
+gem 'less-rails', '~> 2.8.0'
+
+gem 'therubyracer'
+
+gem 'font-awesome-less'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module MeinspiroCom
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    #config.assets.enable = true
+    #config.assets.compile = false
   end
 end
