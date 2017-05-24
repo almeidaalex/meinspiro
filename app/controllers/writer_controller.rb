6e60f0c107c168a5ab1c8589521bc82a8567ca73
alex.almeida <adeno.si@gmail.com>
2017-05-24 04:16:42 -0300

    mais duas alteraçoes

--- app/controllers/writer_controller.rb
+++ app/controllers/writer_controller.rb
@@ -1,13 +1,14 @@
 class WriterController < ApplicationController
     layout 'writer'
     protect_from_forgery with: :exception
-
+    http_basic_authenticate_with name: "gamaray", password: "Grupo08", except: :index
 
     def index
-        
+        @post = Post.new
     end
 
     def new 
+        @post = Post.new
         @allPosts = Post.all.order(created_at: :desc)
     end
 end
