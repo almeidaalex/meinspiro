7d7693927ae29e7afa44ced164dd1c7d17162a6e
alex.almeida <adeno.si@gmail.com>
2017-05-25 03:54:14 -0300

    Comecei a fazer a exportacao de csv dos leas

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,13 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+// $(document).ready(function() {
+//   $.ajaxSetup({ cache: true });
+//   $.getScript('//connect.facebook.net/en_US/sdk.js', function(){
+//     FB.init({
+//       appId: '1440525926254173',
+//       version: 'v2.7' // or v2.1, v2.2, v2.3, ...
+//     });     
+//     $('#loginbutton,#feedbutton').removeAttr('disabled');
+//     FB.getLoginStatus(updateStatusCallback);
+//   });
+// });
--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,3 +1,6 @@
 class Lead < ApplicationRecord
     
+    def to_csv
+        "#{email},#{firstName},#{userIP},#{created_at}"
+    end
 end
