46a6c6212433000dbf6158348a64f0b4741b6e6c
alex.almeida <adeno.si@gmail.com>
2017-05-26 04:50:44 -0300

    Nao fiz nada

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,8 @@ module MeinspiroCom
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.active_record.default_timezone = :utc
-    config.time_zone = 'Brasilia'
+    # config.active_record.default_timezone = :utc
+     config.time_zone = 'Brasilia'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
